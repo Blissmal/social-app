33bd3166d22878dcd4b66477afb3998b7e7fc805
Blissmal <malutibethuel@gmail.com>
2025-02-23 01:50:05 +0300

    Toggle dark and light theme and some ui from shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>Hello from dashboard</h1>
+    <div className="m-4">
+      <SignedOut>
+        <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <ModeToggle />
+      <Button variant="secondary">Click Me</Button>
+    </div>
   );
 }
