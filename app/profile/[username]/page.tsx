4941f98e06ae6d4de7b1bee6e8a2de2c45d9b056
Blissmal <malutibethuel@gmail.com>
2025-02-24 00:41:21 +0300

    Metadata updating

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,8 +1,26 @@
+import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action';
+import { notFound } from 'next/navigation';
 import React from 'react'
 
-const ProfilePage = ({params}: {params: { username: string }}) => {
+export async function generateMetadata({ params }: {params: {username: string}}) {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return;
+  return {
+    title: `${user.username ?? user.name}`,
+    description: user.bio  || `Check out ${user.username}'s profile`,
+  };
+}
 
-    
+const ProfilePage = async ({params}: {params: { username: string }}) => {
+
+    const user = await getProfileByUsername(params.username);
+    if (!user) return notFound();
+
+    const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+      getUserPosts(user.id),
+      getUserLikedPosts(user.id),
+      isFollowing(user.id),
+    ]); 
 
     return (
     <div>
