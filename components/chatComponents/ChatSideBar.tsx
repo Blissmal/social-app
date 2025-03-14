6239444b1cf8120e24c6f6769f98922146fcb391
Blissmal <malutibethuel@gmail.com>
2025-03-15 01:41:50 +0300

    code update

--- components/chatComponents/ChatSideBar.tsx
+++ components/chatComponents/ChatSideBar.tsx
@@ -14,7 +14,7 @@ const Sidebar = async () => {
           <span className="font-medium hidden lg:block">Users</span>
         </div>
         {/* TODO: Online filter toggle */}
-        <div className="mt-3 hidden lg:flex items-center gap-2">
+        {/* <div className="mt-3 hidden lg:flex items-center gap-2">
           <label className="cursor-pointer flex items-center gap-2">
             <input
               type="checkbox"
@@ -25,7 +25,7 @@ const Sidebar = async () => {
             <span className="text-sm">Show online only</span>
           </label>
           <span className="text-xs text-zinc-500">10 online)</span>
-        </div>
+        </div> */}
       </div>
 
       <div className="overflow-y-auto w-full py-3">
