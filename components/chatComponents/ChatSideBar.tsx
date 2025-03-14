e466a4702a29a51c7b96961dad20dceb349461c0
Blissmal <malutibethuel@gmail.com>
2025-03-14 17:57:43 +0300

    user fetch update

--- components/chatComponents/ChatSideBar.tsx
+++ components/chatComponents/ChatSideBar.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react";
 import { Users } from "lucide-react";
+import { getUsersForSidebar } from "@/actions/chat.action";
 
-const Sidebar = () => {
-  
+const Sidebar = async () => {
+
+  const users = await getUsersForSidebar();
+  console.log(users)
+
   return (
     <aside className="h-full w-20 lg:w-72 border-r border-base-300 flex flex-col transition-all duration-200">
       <div className="border-b border-base-300 w-full p-5">
@@ -16,7 +20,7 @@ const Sidebar = () => {
             <input
               type="checkbox"
               checked
-            //   onChange={(e) => setShowOnlineOnly(e.target.checked)}
+              //   onChange={(e) => setShowOnlineOnly(e.target.checked)}
               className="checkbox checkbox-sm"
             />
             <span className="text-sm">Show online only</span>
@@ -26,39 +30,35 @@ const Sidebar = () => {
       </div>
 
       <div className="overflow-y-auto w-full py-3">
-          <button
-            // onClick={() => setSelectedUser(user)}
-            className={`
+        <button
+          // onClick={() => setSelectedUser(user)}
+          className={`
               w-full p-3 flex items-center gap-3
               hover:bg-base-300 transition-colors
               
             `}
-          >
-            <div className="relative mx-auto lg:mx-0">
-              <img
-                src="/avatar.png"
-                className="size-12 object-cover rounded-full"
-              />
-                <span
-                  className="absolute bottom-0 right-0 size-3 bg-green-500 
+        >
+          <div className="relative mx-auto lg:mx-0">
+            <img
+              src="/avatar.png"
+              className="size-12 object-cover rounded-full"
+            />
+            <span
+              className="absolute bottom-0 right-0 size-3 bg-green-500 
                   rounded-full ring-2 ring-zinc-900"
-                />
-              
-            </div>
+            />
+          </div>
 
-            {/* User info - only visible on larger screens */}
-            <div className="hidden lg:block text-left min-w-0">
+          {/* User info - only visible on larger screens */}
+          {users.map((user) => (
+            <div key={user.id} className="hidden lg:block text-left min-w-0">
               <div className="font-medium truncate">Blissmal</div>
-              <div className="text-sm text-zinc-400">
-                online
-              </div>
+              <div className="text-sm text-zinc-400">online</div>
             </div>
-          </button>
-       
+          ))}
+        </button>
 
-        
-          <div className="text-center text-zinc-500 py-4">No online users</div>
-        
+        <div className="text-center text-zinc-500 py-4">No online users</div>
       </div>
     </aside>
   );
