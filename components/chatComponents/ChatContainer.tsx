e46eb37b21bb6c1b3a6e15ce3c1898f4dc85d791
Blissmal <malutibethuel@gmail.com>
2025-03-15 00:46:02 +0300

    code refactor

--- components/chatComponents/ChatContainer.tsx
+++ components/chatComponents/ChatContainer.tsx
@@ -39,8 +39,6 @@ const ChatContainer = async ({ username }: { username: string }) => {
   return (
     <div className="flex-1 flex flex-col overflow-auto">
       <ChatHeader username={username} />
-
-      {/* Chat Messages */}
       <div className="flex-1 overflow-y-auto p-4 space-y-4">
         {messages.length === 0 ? (
           <div className="text-center text-gray-500">No messages yet.</div>
