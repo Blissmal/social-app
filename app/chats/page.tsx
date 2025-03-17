ca30dd841585b0c2808e5d45be6a0b3b6185cd8b
Blissmal <malutibethuel@gmail.com>
2025-03-17 19:10:00 +0300

    Added metadata to the chats route

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,10 +1,15 @@
 import NoChatSelected from "@/components/chatComponents/NoChatSelected";
+import { Metadata } from "next";
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: `Chats | bls Socially`,
+    description: `Stay connected with your friends and groups on bls Socially.`,
+  };
+}
 
 const HomePage = () => {
-
-  return (
-    <NoChatSelected />
-  );
+  return <NoChatSelected />;
 };
+
 export default HomePage;
