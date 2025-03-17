71f62647dd8131dcf688297cf3efc11148fc4efc
Blissmal <malutibethuel@gmail.com>
2025-03-17 19:05:46 +0300

    Removed unused imports

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -1,5 +1,3 @@
-import ChatContainer from "@/components/chatComponents/ChatContainer";
-import Sidebar from "@/components/chatComponents/ChatSideBar";
 import NoChatSelected from "@/components/chatComponents/NoChatSelected";
 
 
