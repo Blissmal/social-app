d8577a68a06545aa97f7aff339961e485a67c776
Blissmal <malutibethuel@gmail.com>
2025-03-19 01:08:03 +0300

    revalidating the main page to update the image

--- actions/user.action.ts
+++ actions/user.action.ts
@@ -29,7 +29,7 @@ export const updateUserImage = async () => {
       },
     });
     console.log("User image updated");
-    revalidatePath(`/profile/${user.username}`);
+    revalidatePath("/");
     
 
   } catch (error) {
