85637cb13eed6e6a869f137cfb83120483b3def4
Blissmal <malutibethuel@gmail.com>
2025-03-22 01:09:12 +0300

    code format

--- app/profile/[username]/page.tsx
+++ app/profile/[username]/page.tsx
@@ -1,36 +1,48 @@
-import { getProfileByUsername, getUserLikedPosts, getUserPosts, isFollowing } from '@/actions/profile.action';
-import { notFound } from 'next/navigation';
-import React from 'react'
-import ProfilePageClient from './ProfilePageClient';
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import React from "react";
+import ProfilePageClient from "./ProfilePageClient";
 
-export async function generateMetadata({ params }: {params: {username: string}}) {
+export async function generateMetadata({
+  params,
+}: {
+  params: { username: string };
+}) {
   const user = await getProfileByUsername(params.username);
   if (!user) return;
   return {
     title: `${user.username ?? user.name}`,
-    description: user.bio  || `Check out ${user.username}'s profile`,
+    description: user.bio || `Check out ${user.username}'s profile`,
   };
 }
 
-const ProfilePageServer = async ({params}: {params: { username: string }}) => {
+const ProfilePageServer = async ({
+  params,
+}: {
+  params: { username: string };
+}) => {
+  const user = await getProfileByUsername(params.username);
+  if (!user) return notFound();
 
-    const user = await getProfileByUsername(params.username);
-    if (!user) return notFound();
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
 
-    const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
-      getUserPosts(user.id),
-      getUserLikedPosts(user.id),
-      isFollowing(user.id),
-    ]); 
-
-    return (
-    <ProfilePageClient 
+  return (
+    <ProfilePageClient
       user={user}
       posts={posts}
       likedPosts={likedPosts}
       isFollowing={isCurrentUserFollowing}
     />
-  )
-}
+  );
+};
 
 export default ProfilePageServer;
