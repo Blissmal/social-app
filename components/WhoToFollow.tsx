2adbcd3ac5605daeb3136764348e83ca14eff7cb
Blissmal <malutibethuel@gmail.com>
2025-04-02 23:50:22 +0300

    code update

--- components/WhoToFollow.tsx
+++ components/WhoToFollow.tsx
@@ -1,4 +1,4 @@
-import { getRandomUsers } from "@/actions/user.action"
+import { getRandomUsers } from "@/actions/user.action";
 import { use } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
 import Link from "next/link";
@@ -6,17 +6,20 @@ import { Avatar, AvatarImage } from "./ui/avatar";
 import FollowButton from "./FollowButton";
 
 const WhoToFollow = async () => {
-    const users = await getRandomUsers();
-    if (users.length === 0) return null;
+  const users = await getRandomUsers();
+  if (users.length === 0) return null;
   return (
     <Card>
-        <CardHeader>
-            <CardTitle>Who to follow</CardTitle>
-        </CardHeader>
-        <CardContent>
+      <CardHeader>
+        <CardTitle>Who to follow</CardTitle>
+      </CardHeader>
+      <CardContent>
         <div className="space-y-4">
           {users.map((user) => (
-            <div key={user.id} className="flex gap-2 items-center justify-between ">
+            <div
+              key={user.id}
+              className="flex gap-2 items-center justify-between "
+            >
               <div className="flex items-center gap-1">
                 <Link href={`/profile/${user.username}`}>
                   <Avatar>
@@ -24,11 +27,16 @@ const WhoToFollow = async () => {
                   </Avatar>
                 </Link>
                 <div className="text-xs">
-                  <Link href={`/profile/${user.username}`} className="font-medium cursor-pointer">
+                  <Link
+                    href={`/profile/${user.username}`}
+                    className="font-medium cursor-pointer"
+                  >
                     {user.name}
                   </Link>
                   <p className="text-muted-foreground">@{user.username}</p>
-                  <p className="text-muted-foreground">{user._count.followers} followers</p>
+                  <p className="text-muted-foreground">
+                    {user._count.followers} followers
+                  </p>
                 </div>
               </div>
               <FollowButton userId={user.id} />
@@ -37,7 +45,7 @@ const WhoToFollow = async () => {
         </div>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default WhoToFollow
+export default WhoToFollow;
